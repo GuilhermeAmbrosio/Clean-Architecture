122ee562ca5af69dcc45a3ab3c0cdcc87c014c6d
Guilherme Capucci Ambrosio <guilherme.ambrosio@apte.com.br>
2023-02-02 13:27:26 -0300

    Criando lista de telefones e método para adicionar telefone.

--- Escola/src/main/java/br/com/guilherme/Aluno.java
+++ Escola/src/main/java/br/com/guilherme/Aluno.java
@@ -1,9 +1,17 @@
 package br.com.guilherme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 
     private Cpf cpf;
     private String nome;
     private Email email;
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public void adicionarTelefone(String ddd, String telefone) {
+        this.telefones.add(new Telefone(ddd, telefone));
+    }
 
 }
