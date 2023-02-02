981e652f82102f66e4dff1f5f9f0b32706acb421
Guilherme Capucci Ambrosio <guilherme.ambrosio@apte.com.br>
2023-02-02 11:47:52 -0300

    Criando a entity Aluno e os Value Objects Cpf e Email.

---
+++ Escola/src/main/java/br/com/guilherme/Cpf.java
@@ -0,0 +1,12 @@
+package br.com.guilherme;
+
+public class Cpf {
+    private String numero;
+
+    public Cpf(String numero) {
+        if (numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+        this.numero = numero;
+    }
+}
